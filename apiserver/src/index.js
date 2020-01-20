2967035692107e1d858d7e9494a0bd2aa6d842a0
Steve <40970351+funnyjerry@users.noreply.github.com>
2020-01-20 15:55:17 +0800

    remove test project

--- apiserver/src/index.js
+++ apiserver/src/index.js
@@ -85,62 +85,18 @@ server.applyMiddleware({ app, path: '/graphql' });
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
-const isTest = !!process.env.TEST_DATABASE_URL;
-const isProduction = process.env.NODE_ENV === 'production';
 const port = process.env.PORT || 8000;
 
 connectDb().then(async () => {
-  if (isTest || isProduction) {
+
     // reset database
     await Promise.all([
       models.User.deleteMany({}),
       models.Message.deleteMany({}),
     ]);
 
-    createUsersWithMessages(new Date());
-  }
-
   httpServer.listen({ port }, () => {
     console.log(`Apollo Server on http://localhost:${port}/graphql`);
   });
 });
 
-const createUsersWithMessages = async date => {
-  const user1 = new models.User({
-    username: 'rwieruch',
-    email: 'hello@robin.com',
-    password: 'rwieruch',
-    role: 'ADMIN',
-  });
-
-  const user2 = new models.User({
-    username: 'ddavids',
-    email: 'hello@david.com',
-    password: 'ddavids',
-  });
-
-  const message1 = new models.Message({
-    text: 'Published the Road to learn React',
-    createdAt: date.setSeconds(date.getSeconds() + 1),
-    userId: user1.id,
-  });
-
-  const message2 = new models.Message({
-    text: 'Happy to release ...',
-    createdAt: date.setSeconds(date.getSeconds() + 1),
-    userId: user2.id,
-  });
-
-  const message3 = new models.Message({
-    text: 'Published a complete ...',
-    createdAt: date.setSeconds(date.getSeconds() + 1),
-    userId: user2.id,
-  });
-
-  await message1.save();
-  await message2.save();
-  await message3.save();
-
-  await user1.save();
-  await user2.save();
-};
--- apiserver/src/models/index.js
+++ apiserver/src/models/index.js
@@ -4,13 +4,6 @@ import User from './user';
 import Message from './message';
 
 const connectDb = () => {
-  if (process.env.TEST_DATABASE_URL) {
-    return mongoose.connect(
-      process.env.TEST_DATABASE_URL,
-      { useNewUrlParser: true },
-    );
-  }
-
   if (process.env.DATABASE_URL) {
     return mongoose.connect(
       process.env.DATABASE_URL,
